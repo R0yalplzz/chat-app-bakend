8b852073250a6d3904be3dfed2b8613d621fb55d
Dibin Rana Magar <royalplz0@gmail.com>
2026-07-03 15:38:14 +0545

    implement socket

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express, { json } from "express";
+import http from "http";
 import { port } from "./src/config.js";
+import { Server } from "socket.io";
 import connectToMongoDb from "./src/connectToDb/connectToMongoDb.js";
 import pageNotFoundMiddleware from "./src/middleware/pageNotFoundMiddleware.js";
 import authRoutes from "./src/routes/authRoutes.js";
@@ -10,9 +12,9 @@ import messageRoutes from "./src/routes/messageRoutes.js";
 
 const app = express();
 
-app.listen(port, () => {
-  console.log(`application is listening at port ${port}`);
-  connectToMongoDb();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
 });
 
 app.use(express.static("public")); //to make public folder as static folder
@@ -26,6 +28,24 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+io.on("connection", (socket) => {
+  socket.on("join-room", (userid) => {
+    socket.join(userid);
+  });
+
+  socket.on("send-message", (message) => {
+    io
+      .to(message.members[0])
+      .to(message.members[1])
+      .emit("receive-message", message);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`application is listening at port ${port}`);
+  connectToMongoDb();
+});
+
 app.use("", pageNotFoundMiddleware);
 app.use((err, req, res, next) => {
   res.json({
@@ -33,5 +53,3 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
-
-/* Goat. */
