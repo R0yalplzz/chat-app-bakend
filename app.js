d8c65f1208de5fe370cc3accdc5c249229046a05
Dibin Rana Magar <royalplz0@gmail.com>
2026-07-08 15:10:57 +0545

    implement clear-unread-message

--- app.js
+++ app.js
@@ -34,10 +34,19 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send-message", (message) => {
-    io
-      .to(message.members[0])
+    io.to(message.members[0])
       .to(message.members[1])
       .emit("receive-message", message);
+  });
+
+  socket.on("clear-unread-messages", (data) => {
+    io.to(data.members[0])
+      .to(data.members[1])
+      .emit("message-count-cleared", data);
+  });
+
+  socket.on("user-typing", (data) => {
+    io.to(data.members[0]).to(data.members[1]).emit("started-typing", data);
   });
 });
 
